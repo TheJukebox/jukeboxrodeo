b33e836c93713f0c5137c2da047de3e39ceccff2
the jukebox <5196313+TheJukebox@users.noreply.github.com>
2025-03-20 15:22:00 +1100

    Fix base

---
+++ docs/build/_app/immutable/entry/start.BH9W3pDF.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.mP1O_puj.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const base = process.env.NODE_ENV === 'production' ? '/jukeboxrodeo' : '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
+        paths: {
+            base,
+        },
 		adapter: adapter(),
         prerender: {
-            entries: ['*']
+            entries: ['*'],
+            handleHttpError: ({ status, path }) => {
+               if (status === 404) {
+                   console.warn(`Ignoring 404 for ${path}`);
+                   return;
+               }
+               throw new Error(`Prerendering failed at ${path} with status ${status}`);
+            }
         }
 	}
 };
